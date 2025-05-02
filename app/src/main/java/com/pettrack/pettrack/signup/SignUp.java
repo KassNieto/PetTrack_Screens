a3f6c13bce3dd62c13dd960c64042e7993ffb26d
AxFerCg13 <axfervagar@gmail.com>
2025-05-02 13:45:06 -0600

    refactor: refactor singup view

--- app/src/main/java/com/pettrack/pettrack/signup/SignUp.java
+++ app/src/main/java/com/pettrack/pettrack/signup/SignUp.java
@@ -1,13 +1,10 @@
 package com.pettrack.pettrack.signup;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.pettrack.pettrack.R;
 
 public class SignUp extends AppCompatActivity {
@@ -15,8 +12,9 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+
+        // Manejo de insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
