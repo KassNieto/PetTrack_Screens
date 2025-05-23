7364d3f544b3bfdb579787332dbe726c9a04d0ff
Daniela Zaragoza <daniela.zaragoza@uaq.mx>
2025-05-23 11:09:33 -0600

    feat: add cartillaVacunacion endpoints

--- app/src/main/java/com/pettrack/pettrack/api/ApiService.java
+++ app/src/main/java/com/pettrack/pettrack/api/ApiService.java
@@ -6,6 +6,8 @@ import com.pettrack.pettrack.models.Mascota;
 import com.pettrack.pettrack.models.User;
 import com.pettrack.pettrack.models.signup.RegisterRequest;
 import com.pettrack.pettrack.models.signup.ApiResponse;
+import com.pettrack.pettrack.models.cartillavacunacion.Vacuna;
+import com.pettrack.pettrack.models.cartillavacunacion.Desparasitacion;
 
 import java.util.List;
 
@@ -16,15 +18,37 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ApiService {
+    // Usuarios
     @POST("/usuarios/login")
     Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
 
-    @POST("/usuarios") // Endpoint para registro
+    @POST("/usuarios")
     Call<ApiResponse> registerUser(@Body RegisterRequest registerRequest);
 
-    @GET("usuarios/{id}")
-    Call<User> getUserById(@Path("id") int userId); // Cambiado a int
+    @GET("/usuarios/{id}")
+    Call<User> getUserById(@Path("id") int userId);
 
-    @GET("usuarios/{id}/mascotas")
+    // Mascotas
+    @GET("/usuarios/{id}/mascotas")
     Call<List<Mascota>> getMascotasByUsuarioId(@Path("id") int usuarioId);
+
+    // Vacunas
+    @GET("/mascotas/{id}/vacunas")
+    Call<List<Vacuna>> getVacunasByMascotaId(@Path("id") int mascotaId);
+
+    @GET("/mascotas/{id}/vacunas/{vacunaId}")
+    Call<Vacuna> getVacunaById(@Path("id") int mascotaId, @Path("vacunaId") int vacunaId);
+
+    @POST("/mascotas/{id}/vacunas")
+    Call<Vacuna> agregarVacuna(@Path("id") int mascotaId, @Body Vacuna vacuna);
+
+    // Desparasitaciones
+    @GET("/mascotas/{id}/desparasitaciones")
+    Call<List<Desparasitacion>> getDesparasitacionesByMascotaId(@Path("id") int mascotaId);
+
+    @GET("/desparasitaciones/{id}")
+    Call<Desparasitacion> getDesparasitacionById(@Path("id") int desparasitacionId);
+
+    @POST("/mascotas/{id}/desparasitaciones")
+    Call<Desparasitacion> agregarDesparasitacion(@Path("id") int mascotaId, @Body Desparasitacion desparasitacion);
 }
